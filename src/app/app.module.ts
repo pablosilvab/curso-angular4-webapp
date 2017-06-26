a79dc54018801aa91befd0c1b1ed40b3830def11
pablosilvab <pablonicolassilvabravo@gmail.com>
2017-06-25 23:53:46 -0400

    Configuración de router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//  Rutas
+import { routing, appRoutingProviders } from './app.routing';
+
 //  Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
@@ -16,9 +19,10 @@ import { HomeComponent } from './components/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
