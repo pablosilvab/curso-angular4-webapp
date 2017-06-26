7c57203836eb687a0680e233a77db9b5677501d9
pablosilvab <pablonicolassilvabravo@gmail.com>
2017-06-26 12:24:46 -0400

    Guardar producto en BD

--- src/app/components/producto-add.component.ts
+++ src/app/components/producto-add.component.ts
@@ -13,7 +13,11 @@ export class ProductoAddComponent {
     public titulo: string;
     public producto: Producto;
 
-    constructor() {
+    constructor(
+        private _productoService: ProductoService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {
         this.titulo = 'Crear un nuevo producto';
         this.producto = new Producto(0,'','',0,'');
     }
@@ -24,6 +28,18 @@ export class ProductoAddComponent {
 
     onSubmit(){
         console.log(this.producto);
+        this._productoService.addProducto(this.producto).subscribe(
+            response => {
+                if (response.code == 200){
+                    this._router.navigate(['/home']);
+                } else {
+                    console.log(response);
+                }
+            },
+            error => {
+                console.log(<any> error);
+            }
+        );
     }
 
 }
--- src/app/services/producto.services.ts
+++ src/app/services/producto.services.ts
@@ -18,4 +18,13 @@ export class ProductoService{
     getProductos(){
         return this._http.get(this.url+'productos').map(res => res.json());
     }
+
+    addProducto(producto: Producto){
+        let json = JSON.stringify(producto);
+        let params = 'json='+json;
+        let headers = new Headers({'Content-type':'application/x-www-form-urlencoded'});    
+        
+        return this._http.post(this.url+'productos', params, {headers: headers})
+                         .map(res => res.json());
+    }
 }
